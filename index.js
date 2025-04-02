cd81624712a47d508bf71a2dc79c2b4c5148b60e
juancai0104 <2220201024@estudiantesunibague.edu.co>
2025-04-02 11:11:53 -0500

    Change token to cookie

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
-
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors')
+
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
