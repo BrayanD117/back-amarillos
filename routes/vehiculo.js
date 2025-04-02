3702f604737af796abce8a2661927601e4193312
Brayan Patiño <brayandavid1928@gmail.com>
2025-04-02 09:31:31 -0500

    Remove isAdmin validation in vehicle routes

--- routes/vehiculo.js
+++ routes/vehiculo.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const vehiclesController = require('../controllers/vehiculo');
 const { verifyToken, isAdmin } = require('../middlewares/autenticacionJwt');
 
-router.post('/', verifyToken, isAdmin, vehiclesController.createVehicle);
-router.get('/', verifyToken, isAdmin, vehiclesController.getAllVehicles);
-router.get('/opciones', verifyToken, isAdmin, vehiclesController.getVehicleOptions);
-router.get('/relaciones/:id', verifyToken, isAdmin, vehiclesController.getVehicleWithRelations);
+router.post('/', verifyToken, vehiclesController.createVehicle);
+router.get('/', verifyToken, vehiclesController.getAllVehicles);
+router.get('/opciones', verifyToken, vehiclesController.getVehicleOptions);
+router.get('/relaciones/:id', verifyToken, vehiclesController.getVehicleWithRelations);
 router.get('/:id', verifyToken, vehiclesController.getVehicleById);
-router.put('/:id', verifyToken, isAdmin, vehiclesController.updateVehicle);
-router.delete('/:id', verifyToken, isAdmin, vehiclesController.deleteVehicle);
+router.put('/:id', verifyToken, vehiclesController.updateVehicle);
+router.delete('/:id', verifyToken, vehiclesController.deleteVehicle);
 
 module.exports = router;
